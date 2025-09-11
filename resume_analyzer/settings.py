ddb73a45ef9959fb53ec0595b6191c0750cd9783
Pranjal Singh <okpranjalsingh@gmail.com>
2025-09-11 23:51:07 +0530

    Add Whitenoise for static files

--- resume_analyzer/settings.py
+++ resume_analyzer/settings.py
@@ -161,6 +161,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -214,6 +215,8 @@ USE_TZ = True
 # Static files
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Media files
 MEDIA_URL = '/media/'
